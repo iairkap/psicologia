1208b7ad48f4518e4b5001a19de611a47d131589
Adri-ESM <anggivmorales@gmail.com>
2023-08-17 21:24:31 -0400

    section three from home in main there are services and modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,11 +47,33 @@ export default function Home() {
         {/* Sección 2 */}
         <section className="flex p-4">
           <div className="w-1/2 pl-4">
-            <p>Texto de la sección 2</p>
+            <div>
+              <p>
+                <span className="text-dark-pink">¡Hola!</span> Soy una psicóloga
+                perinatal dedicada a brindar apoyo emocional y psicológico a las
+                personas durante el período de embarazo, parto y posparto.
+              </p>
+            </div>
+            <div className="mt-10">
+              <p>
+                Mi objetivo es acompañarte en este viaje emocionalmente
+                significativo, proporcionando orientación y herramientas para
+                enfrentar desafíos como ansiedad, depresión, cambios de estado
+                de ánimo y ajustes a la nueva etapa de la maternidad/paternidad.
+              </p>
+            </div>
+            <div className="mt-10">
+              <p>
+                Juntos, podemos trabajar para fortalecer tu bienestar emocional
+                y promover un ambiente saludable para ti y tu familia. Estoy
+                aquí para ayudarte a navegar por este emocionante y
+                transformador capítulo de tu vida.
+              </p>
+            </div>
           </div>
           <div className="w-1/2">
             <Image
-              src="https://images.pexels.com/photos/5207589/pexels-photo-5207589.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src="https://images.pexels.com/photos/7631944/pexels-photo-7631944.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Imagen de la sección 2"
               width={500}
               height={300}
@@ -60,20 +82,32 @@ export default function Home() {
         </section>
 
         {/* Sección 3 */}
-        <section className="flex bg-dark-pink p-4">
-          <div className="w-1/4 text-white">
-            <p>Texto de la sección 3 - Lado izquierdo</p>
-            <button className="text-white bg-dark-pink">Botón</button>
+        <section className="bg-dark-pink">
+          <div className="w-full text-white py-4 px-4 mb-4">
+            <h2>SERVICIOS</h2>
           </div>
-          <div className="w-1/2"></div>
-          <div className="w-1/4 text-white">
-            <p>Texto de la sección 3 - Lado medio</p>
-            <button className="text-white bg-dark-pink">Botón</button>
-          </div>
-          <div className="w-1/2"></div>
-          <div className="w-1/4 text-white">
-            <p>Texto de la sección 3 - Lado derecho</p>
-            <button className="text-white bg-dark-pink">Botón</button>
+          <div className="flex p-4">
+            <div className="w-1/4 text-white">
+              <Image
+                src="/images/heart.png"
+                alt="Icono de corazón"
+                width={50}
+                height={50}
+              />
+              <h3>Texto de la sección 3</h3>
+              <h3>Lado izquierdo</h3>
+              <button className="text-white bg-dark-pink">Botón</button>
+            </div>
+            <div className="w-1/2"></div>
+            <div className="w-1/4 text-white">
+              <p>Texto de la sección 3 - Lado medio</p>
+              <button className="text-white bg-dark-pink">Botón</button>
+            </div>
+            <div className="w-1/2"></div>
+            <div className="w-1/4 text-white">
+              <p>Texto de la sección 3 - Lado derecho</p>
+              <button className="text-white bg-dark-pink">Botón</button>
+            </div>
           </div>
         </section>
 
